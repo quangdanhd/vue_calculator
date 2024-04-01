908e6c824bbc4e69c97eb1af9a0bb0e545581d6d
quangdanhd <quangdanhd@gmail.com>
2024-04-01 21:55:50 +0700

    user interface

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <calculator-background />
+  <calculator-wrapper />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalculatorBackground from "@/components/CalculatorBackground.vue";
+import CalculatorWrapper from "@/components/CalculatorWrapper.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CalculatorBackground,
+    CalculatorWrapper,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
